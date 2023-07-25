b3c3be26ad83f656a13f4e5eda7c05310b42d08e
Filip Condac <filipcondac@gmail.com>
2023-07-25 14:31:28 +0100

    Added eslint + better page structure

---
+++ src/pages/about.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <h1 className="text-green-500">About</h1>
+      <p className="text-green-500">This is the about page</p>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 export default function Page() {
   return <h1 className="text-green-500">Hello, Next.js!</h1>;
 }
